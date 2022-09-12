8bcba3f7eb3a1db00b040ddf4339236895900b5c
Wenderson Santos <guto15santos@gmail.com>
2022-09-12 09:17:07 -0300

    Questao 13

---
+++ src/main/java/com/capgemini/exerciciosjava/Questao13.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package com.capgemini.exerciciosjava;
+
+/**
+ * @author WENDERSON
+ * 13. Faça um algoritmo que receba um número e mostre uma mensagem caso este número seja maior que 10;
+ *
+ */
+public class Questao13 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		int n = 11;
+		
+		if(n > 10) {
+			System.out.println("Maior que 10");
+		}
+
+	}
+
+}
