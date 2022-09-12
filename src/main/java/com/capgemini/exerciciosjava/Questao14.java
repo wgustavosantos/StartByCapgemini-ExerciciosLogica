3b8a191522b0c79b40317a7b671491f12ce22a7d
Wenderson Santos <guto15santos@gmail.com>
2022-09-12 09:19:07 -0300

    Questao 14

---
+++ src/main/java/com/capgemini/exerciciosjava/Questao14.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package com.capgemini.exerciciosjava;
+
+/**
+ * @author WENDERSON 14. Escreva um algoritmo que leia dois valores inteiro
+ *         distintos e informe qual é o maior;
+ *
+ */
+public class Questao14 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		int n1 = 2, n2 = 3;
+		
+		System.out.println(n1 > n2 ? n1 : n2);
+
+	}
+
+}
